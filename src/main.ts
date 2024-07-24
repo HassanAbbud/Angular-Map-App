f2f2924885f5a419fbeabdcdbb13dd95ae9f42b2
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-24 13:37:09 -0600

    Get user geolocation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if ( !navigator.geolocation ) {
+  alert('Browser does not support Geolocation');
+  throw new Error('Browser does not support Geolocation');
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
