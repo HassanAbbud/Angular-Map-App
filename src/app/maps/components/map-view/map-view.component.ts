51d81b989e9b43f4442194dc3ad0443b2714418d
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-24 14:08:12 -0600

    Import leaflet & map test

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,15 +1,29 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
 import { PlacesService } from '../../services';
+import * as L from 'leaflet'
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.css'
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements AfterViewInit {
   private placesService = inject(PlacesService);
 
-  ngOnInit(): void {
-    console.log(this.placesService.useLocation)
+  public map?: L.Map;
+
+  ngAfterViewInit(): void {
+    this.initMap();
   }
+
+  public initMap(){
+    console.log(this.placesService.useLocation)
+    this.map = L.map('map').setView([51.505, -0.09], 13)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+  }
+
+
 }
