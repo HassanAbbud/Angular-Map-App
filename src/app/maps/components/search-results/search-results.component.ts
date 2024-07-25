3f1ba96619e05be930639b5b85f5375a83445dc4
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-25 09:30:15 -0600

    Fly to searched place

--- src/app/maps/components/search-results/search-results.component.ts
+++ src/app/maps/components/search-results/search-results.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { PlacesService } from '../../services/places.service';
+import { MapService } from '../../services';
+import { LatLngExpression } from 'leaflet';
+import { SearchResult } from 'leaflet-geosearch/dist/providers/provider.js';
 
 @Component({
   selector: 'app-search-results',
@@ -8,11 +11,22 @@ import { PlacesService } from '../../services/places.service';
 })
 export class SearchResultsComponent {
   private placesService = inject(PlacesService);
+  private mapService = inject(MapService);
+
+  public selectedId: string = "";
 
   get places(){
     return this.placesService.places
   }
   get isLoadingPlaces(){
     return this.placesService.isLoadingPlaces
+  }
+
+  flyTo(place: SearchResult) {
+
+    this.selectedId = place.label
+    const currentLatLng: LatLngExpression = [place.y, place.x];
+
+    this.mapService.flyTo(currentLatLng)
   }
 }
